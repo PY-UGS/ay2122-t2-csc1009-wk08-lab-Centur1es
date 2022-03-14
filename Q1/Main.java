65294bbed0e070cff255c256c3c08292911901d4
Centur1es <93638341+Centur1es@users.noreply.github.com>
2022-03-15 00:59:03 +0800

    Delete Q1 directory

--- Q1/Main.java
+++
@@ -1,8 +0,0 @@
-package T8.Q1;
-
-public class Main {
-    public static void main(String[] args) {
-        CircleWithException circle = new CircleWithException();
-        circle.setRadius(-1);
-    }
-}
